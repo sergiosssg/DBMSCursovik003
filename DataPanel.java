2c3197359e0a28fa69c840d063192713df62fb52
sergiosssg <sergiosssg@gmail.com>
2014-04-09 14:04:01 +0300

    Create DataPanel.java

---
+++ DataPanel.java
@@ -0,0 +1,51 @@
+
+
+
+/**
+   This panel displays the contents of a result set.
+*/
+class DataPanel extends JPanel
+{
+   /**
+      Constructs the data panel.
+      @param rs the result set whose contents this panel displays
+   */
+   public DataPanel(ResultSet rs) throws SQLException
+   {
+      fields = new ArrayList<JTextField>();
+      setLayout(new GridBagLayout());
+      GridBagConstraints gbc = new GridBagConstraints();
+      gbc.gridwidth = 1;
+      gbc.gridheight = 1;
+      ResultSetMetaData rsmd = rs.getMetaData();
+      for (int i = 1; i <= rsmd.getColumnCount(); i++)
+      {
+         gbc.gridy = i - 1;
+         String columnName = rsmd.getColumnLabel(i);
+         gbc.gridx = 0;
+         gbc.anchor = GridBagConstraints.EAST;
+         add(new JLabel(columnName), gbc);
+         int columnWidth = rsmd.getColumnDisplaySize(i);
+         JTextField tb = new JTextField(columnWidth);
+         fields.add(tb);
+         gbc.gridx = 1;
+         gbc.anchor = GridBagConstraints.WEST;
+         add(tb, gbc);
+      }
+   }
+   /**
+      Shows a database row by populating all text fields
+      with the column values.
+   */
+   public void showRow(ResultSet rs) throws SQLException
+   {
+      for (int i = 1; i <= fields.size(); i++)
+      {
+         String field = rs.getString(i);
+         JTextField tb = (JTextField) fields.get(i - 1);
+         tb.setText(field);
+      }
+   }
+   private ArrayList<JTextField> fields;
+}
+ 
